75c331f5f17274b850861eed533afabfa651e6d2
Pyramidplac <pyramidd123@gmail.com>
2023-06-19 09:57:10 +0530

    first commit

---
+++ person.js
@@ -0,0 +1,14 @@
+const person = ()=>{
+    const fname="Rajan";
+    const lname="Kathiriya";
+    const age=20;
+    const city="junagadh";
+    
+    return "My Name is "+fname +" "+ lname +"."+ " I am " +age +"Years old. " +"I am From "+city+".";
+}
+
+const cname="Audi";
+const Model="Q7"
+
+export default person;
+export {cname,Model}
